3cc23cb455bc9be2d5c9e7e04e449c463201a799
Denley Hsiao <denley@justtodo.com>
2024-09-02 23:26:52 +0800

    Add config option.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
-#include "math/MathFunctions.h"
+#include "config.h"
 #include <iostream>
+#ifdef USE_MYMATH
+  #include "MathFunctions.h"
+#else
+  #include <cmath>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +16,14 @@ int main(int argc, char *argv[])
 
   double base = atof(argv[1]);
   int exponent = atoi(argv[2]);
+#ifdef USE_MYMATH
+  std::cout << "Use my math library." << std::endl;
   double result = power(base, exponent);
+#else
+  std::cout << "Use standard library." << std::endl;
+  double result = pow(base, exponent);
+#endif
+
   std::cout << base << " ^ " << exponent << " is " << result  << std::endl;
 
   return 0;
